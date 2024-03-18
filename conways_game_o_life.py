0de4245fd2ed89ed5c2539d6df0d93e56768b473
animesh <aninaugai2003@gmail.com>
2024-03-18 20:46:22 +0530

    docs

--- conways_game_o_life.py
+++ conways_game_o_life.py
@@ -1,6 +1,6 @@
 #!../bin/python3
 
-import json
+import toml
 import os
 import pygame
 from collections import defaultdict
@@ -110,7 +110,9 @@ class env():
         ]
 
     def load_pattern(self):
-        pass
+        with open("patterns.toml","a") as f:
+            dct = {"pt":self.live_cells}
+            toml.dump(dct,f)
 
     def save_pattern(self):
         pass
